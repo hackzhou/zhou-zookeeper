a21cdf738477c791ccdc79e53741e83c7092ef0a
hackzhou@foxmail.com <hackzhou@foxmail.com>
2018-04-14 13:47:50 +0800

    更新

---
+++ src/main/java/com/zk/lock/OrderCodeGenerator.java
@@ -0,0 +1,26 @@
+package com.zk.lock;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 简单地分布式自增序列
+ */
+public class OrderCodeGenerator {
+	// 自增长序列
+	private static int i = 0;
+
+	// 按照“年-月-日-小时-分钟-秒-自增长序列”的规则生成订单编号
+	public String getOrderCode() {
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		return sdf.format(now) + ++i;
+	}
+
+	public static void main(String[] args) {
+		OrderCodeGenerator ong = new OrderCodeGenerator();
+		for (int i = 0; i < 10; i++) {
+			System.out.println(ong.getOrderCode());
+		}
+	}
+}
